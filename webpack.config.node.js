d66a7d473c4f8b565e2cc6b8cbbecf3336affa8c
ehg <chris.blower@jumpwire.co.uk>
2016-02-18 19:07:09 +0000

    change noop location

--- webpack.config.node.js
+++ webpack.config.node.js
@@ -74,9 +74,9 @@ module.exports = {
 	plugins: [
 		// Require source-map-support at the top, so we get source maps for the bundle
 		new webpack.BannerPlugin( 'require( "source-map-support" ).install();', { raw: true, entryOnly: false } ),
-		new webpack.NormalModuleReplacementPlugin( /^analytics$/, 'lodash/utility/noop' ), // Depends on BOM
-		new webpack.NormalModuleReplacementPlugin( /^lib\/upgrades\/actions$/, 'lodash/utility/noop' ), // Uses Flux dispatcher
-		new webpack.NormalModuleReplacementPlugin( /^lib\/route$/, 'lodash/utility/noop' ) // Depends too much on page.js
+		new webpack.NormalModuleReplacementPlugin( /^analytics$/, 'lodash/noop' ), // Depends on BOM
+		new webpack.NormalModuleReplacementPlugin( /^lib\/upgrades\/actions$/, 'lodash/noop' ), // Uses Flux dispatcher
+		new webpack.NormalModuleReplacementPlugin( /^lib\/route$/, 'lodash/noop' ) // Depends too much on page.js
 	],
 	externals: getExternals()
 };
